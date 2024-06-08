46eb2e66e55c12b55442596de96eaf3145bc346e
Lee <leeadams5902@gmail.com>
2024-06-08 21:12:55 +0100

    Initial Structure

---
+++ src/Lee1387/Game/Stage/Trait/JoinableTrait.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Lee1387\Game\Stage\Trait;
+
+trait JoinableTrait 
+{
+    
+}
---
+++ src/Lee1387/SkyWars.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lee1387;
+
+use pocketmine\plugin\PluginBase;
+
+class SkyWars extends PluginBase
+{
+    
+}
