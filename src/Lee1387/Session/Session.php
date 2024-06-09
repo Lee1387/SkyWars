c8ce1f2e30badf1ab1d16b43380c6a07cdf6deb1
Lee <leeadams5902@gmail.com>
2024-06-09 19:51:41 +0100

    Game->getAvailableTeams(): Remove method

--- src/Lee1387/Session/Session.php
+++ src/Lee1387/Session/Session.php
@@ -172,7 +172,7 @@ class Session
 
     public function assignTeam(): void 
     {
-        foreach($this->game->getAvailableTeams() as $team) {
+        foreach($this->game->getTeams() as $team) {
             if(!$team->isFull()) {
                 $team->addMember($this);
                 return;
